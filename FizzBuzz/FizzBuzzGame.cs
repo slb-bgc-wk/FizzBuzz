d251ff42b9d67858149cc196a3f750114f70f26d
Ke <kwang97@slb.com>
2023-11-01 14:23:49 +0800

    feat: add abnormal cases handle feature in CountOff function of FizzBuzz class

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -5,6 +5,15 @@
         public string CountOff(int num)
         {
             string output = string.Empty;
+            if (num % 3 == 0)
+            {
+                output += "Fizz";
+            }
+
+            if (num % 5 == 0)
+            {
+                output += "Buzz";
+            }
 
             return output.Length == 0 ? num.ToString() : output;
         }
